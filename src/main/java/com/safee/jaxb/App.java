2022fcf1e6f9c43fd450f388079f0f1e73b2e197
Safeeul Bashir Safee <safee_cuet06@yahoo.com>
2017-06-15 11:49:11 -0500

    [Change Type] Added Final Changes

--- src/main/java/com/safee/jaxb/App.java
+++ src/main/java/com/safee/jaxb/App.java
@@ -4,8 +4,11 @@ import java.io.File;
 
 import javax.xml.bind.JAXBContext;
 import javax.xml.bind.JAXBException;
+import javax.xml.bind.Marshaller;
 import javax.xml.bind.Unmarshaller;
 
+import com.safee.jaxbclasses.Address;
+import com.safee.jaxbclasses.Mark;
 import com.safee.jaxbclasses.Student;
 
 /**
@@ -14,7 +17,8 @@ import com.safee.jaxbclasses.Student;
  */
 public class App {
 	public static void main(String[] args) {
-		App app=new App();
+		App app = new App();
+		app.marshall();
 		app.unmarshall();
 
 	}
@@ -25,12 +29,67 @@ public class App {
 		try {
 			jaxbContext = JAXBContext.newInstance(Student.class);
 			Unmarshaller jaxbUnmarshaller = jaxbContext.createUnmarshaller();
+			//jaxbUnmarshaller.setProperty(Marshaller.JAXB_FORMATTED_OUTPUT, true);
 			Student student = (Student) jaxbUnmarshaller.unmarshal(file);
-			System.out.println(student);
+			System.out.println(student.toString());
 		} catch (JAXBException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
 	}
 
+	public void marshall() {
+		File file = new File("C:/SWInstallation/LoyalWorkspace/myjaxb/src/main/resources/Student.xml");
+		JAXBContext jaxbContext;
+		try {
+			jaxbContext = JAXBContext.newInstance(Student.class);
+			Marshaller jaxbMarshaller = jaxbContext.createMarshaller();
+			// output pretty printed
+			jaxbMarshaller.setProperty(Marshaller.JAXB_FORMATTED_OUTPUT, true);
+
+			 jaxbMarshaller.marshal(getMeAnEmployee(), file);
+			//jaxbMarshaller.marshal(getMeAnEmployee(), System.out);
+		} catch (JAXBException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+
+	}
+
+	public Student getMeAnEmployee() {
+		Student student = new Student();
+		student.setStudentId(1);
+		Address address = new Address();
+		address.setAddressType("HOME");
+		address.setLine1("1026 San Jacinto Dr.");
+		address.setLine2("Apt 1714");
+		address.setCity("Irving");
+		address.setState("TX");
+		address.setZip(75063);
+		address.setCountry("USA");
+		Mark mark = new Mark();
+		mark.setSubject("JAVA");
+		mark.setMark(90);
+		mark.setMaxMark(100);
+		Address address2 = new Address();
+		address2.setAddressType("HOME");
+		address2.setLine1("951 Buena Vista Dr. SE.");
+		address2.setLine2("Apt G201");
+		address2.setCity("Albuquerque");
+		address2.setState("NM");
+		address2.setZip(87106);
+		address2.setCountry("USA");
+		Mark mark2 = new Mark();
+		mark2.setSubject("C++");
+		mark2.setMark(95);
+		mark2.setMaxMark(100);	
+		student.setStudentId(1);
+		student.setFirstName("Safeeul Bashir");
+		student.setLastName("Safee");
+		student.getAddress().add(address);
+		student.getMark().add(mark);
+		student.getAddress().add(address2);
+		student.getMark().add(mark2);
+		return student;
+	}
 }
